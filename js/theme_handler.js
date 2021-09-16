51d6e7b3cdf9d3bf1ffa6fad968e3ac590917d3d
jm11116 <74529681+jm11116@users.noreply.github.com>
2021-09-16 21:16:19 +1000

    Partially added persistence to theme handler

--- js/theme_handler.js
+++ js/theme_handler.js
@@ -1,25 +1,69 @@
+/* Persistent theme handler - NOT YET WORKING */
+
 class ThemeHandler {
     constructor(){
+        this.theme_affected_elements = 
+            "#projects_table, td, th, nav, .form-control, .sidebar, a, body, button, #sign_out";
         this.bindDarkModeButton();
-        this.currentTheme = "light";
-    }
-    saveThemeInLocalStorage(){
-
-    }
-    toggleDarkMode(){
-        if (this.currentTheme === "light"){
-            this.currentTheme = "dark";
-            $("#projects_table, td, th, nav, .form-control, .sidebar, a, body, button, #sign_out").addClass("dark");
-        } else if (this.currentTheme === "dark"){
-            this.currentTheme = "light";
-            $("#projects_table, td, th, nav, .form-control, .sidebar, a, body, button, #sign_out").removeClass("dark");
-        }
+        this.current_setting = localStorage.getItem("theme");
+        this.current_theme = this.getCurrentThemeSetting();
+        this.obeyCurrentThemeSetting();
     }
     bindDarkModeButton(){
-        document.getElementById("theme_toggle").addEventListener("click", (e) => {
-            this.toggleDarkMode();
-            this.currentState = "dark";
+        $("#theme_toggle").click(() => {
+            this.toggleTheme();
         });
+    }
+    toggleTheme(){
+        if (this.current_theme === "light"){
+            this.changeTheme("dark");
+        } else if (this.current_theme === "dark"){
+            this.changeTheme("light");
+        } else {
+            this.changeTheme("light");
+        }
+    }
+    changeTheme(type){
+        if (type === "light"){
+            $(this.theme_affected_elements).removeClass("dark");
+            this.changeThemeSetting("light");
+        } else if (type === "dark"){
+            $(this.theme_affected_elements).addClass("dark");
+            this.changeThemeSetting("dark");
+        }
+    }
+    changeThemeSetting(type){
+        if (type === "light"){
+            localStorage.setItem("theme", "light");
+            this.currentTheme = "light";
+        } else if (type === "dark"){
+            localStorage.setItem("theme", "dark");
+            this.currentTheme = "dark";
+        }
+    }
+    getCurrentThemeSetting(){
+        if (this.current_setting != null){
+            if (this.current_setting === "light"){
+                return "light";
+            } else if (this.current_setting === "dark"){
+                this.changeTheme("dark");
+                return "dark";
+            }
+        } else {
+            return false;
+        }
+    }
+    obeyCurrentThemeSetting(){
+        if (this.current_setting === null){ //If no setting found, set light them as default
+            var theme_setting = localStorage.setItem("theme", "light");
+            return theme_setting;
+        } else if (this.current_setting != null){
+            if (this.current_setting === "light"){
+                this.changeTheme("light");
+            } else if (this.current_setting === "dark"){
+                this.changeTheme("dark");
+            }
+        }
     }
 }
 
